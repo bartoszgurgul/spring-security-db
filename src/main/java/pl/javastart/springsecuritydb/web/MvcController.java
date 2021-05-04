168f028fc5e878f21e04d610b0cd497becbe3af5
bartoszgurgul <bartoszgurgul@gmail.com>
2021-05-04 23:05:43 +0200

    Initial commit

---
+++ src/main/java/pl/javastart/springsecuritydb/web/MvcController.java
@@ -0,0 +1,21 @@
+package pl.javastart.springsecuritydb.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MvcController {
+
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @RequestMapping("/secured")
+    public String securited(){
+        return "secured";
+    }
+
+}
---
+++ src/test/java/pl/javastart/springsecuritydb/SpringSecurityDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.springsecuritydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
